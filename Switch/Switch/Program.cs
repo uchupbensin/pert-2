7416940f26a8b2ef27d7ac9e63651ed7c108fa8b
uchupbensin <arif.kurniawan@students.amikom.ac.id>
2023-03-14 14:49:54 +0700

    commit ke-1

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ini apps sederhana mengguanakan ";
+            Console.Write("Operasi apa yang ingin diterjemahkan \n 1. '+' \n 2. '-' \n 3. 'x' \n 4. '/' \n : ");
+            char Operasi = char.Parse(Console.ReadLine());
+
+            if(Operasi != '+' && Operasi != '-' && Operasi != 'x' && Operasi != '/')
+            {
+                Console.WriteLine("Operasi yang anda masukan tidak tersedia");
+                return;
+            } else
+            switch(Operasi)
+            {
+                case '+':
+                    Console.WriteLine("Operasi penjumlahan");
+                    break;
+                case '-':
+                    Console.WriteLine("Operasi pengurangan");
+                    break;
+                case 'x':
+                    Console.WriteLine("Operasi perkalian");
+                    break;
+                case '/':
+                    Console.WriteLine("Operasi pembagian");
+                    break;
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator";
+
+            //int a = 10; // deklarasi variabel a dg nilai awal 10
+            //int b = 6;  // deklarasi variabel b dg nilai 6
+
+            Console.Write("inputkan nilai a =");
+            int a = int.Parse(Console.ReadLine()); // proses casting
+
+            Console.Write("inputkan nilai b = ");
+            int b = Convert.ToInt32(Console.ReadLine()); // proses casting
+
+            Console.WriteLine(); // tambahkan baris kosong
+
+            Console.WriteLine("Hasil penambah " + a + "+" + b + "=" + Penambahan(a, b));
+            Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
+            Console.WriteLine("Hasil perkalian {0} * {1} = {2}", a, b, perkalian(a, b));
+            Console.WriteLine("Hasil pembagian {0} / {1} = {2}", a, b, pembagian(a, b));
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey(); 
+        }
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int pengurangan (int a, int b)
+        {
+            return a - b;
+        }
+        static int perkalian (int a, int b)
+        {
+            return a * b;
+        }
+        static int pembagian (int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
